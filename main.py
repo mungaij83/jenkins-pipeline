1925111d6fe4fc67314bda395de8db577665638a
mungai <mungaij83@gmail.com>
2019-11-03 14:22:47 +0300

    Fun

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 app=Flask(__name__)
-
+def newFunction():
+    print("Local check")
 if __name__=='__main__':
     print("Starting server: %d"%9089)
     app.run(port=9089)
