1b33f6c701350406223cdf114021f280e10d875a
mungai <mungaij83@gmail.com>
2019-11-03 15:02:44 +0300

    Hello endpoint

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, request, escape
 
+# Init application
 app=Flask(__name__)
+
+# Welcome endpoint
+@app.route('/hello')
 def newFunction():
     print("Local check")
+    name=request.args.get('name','John')
+    return f"Hello , {escape(name)}"
+
+# Initialize application
 if __name__=='__main__':
     print("Starting server: %d"%9089)
     app.run(port=9089)
